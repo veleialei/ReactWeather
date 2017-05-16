80c450e7832f44e62ab7edbcaaa431ac29a2d6ca
Sarah <veleialei@gmail.com>
2017-05-16 15:11:21 -0700

    Add foundation & jQuery

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,12 +1,6 @@
 var React = require('react');
 
-//var About = React.createClass({
-//    render: function (){
-//        return (
-//            <h3>About Component</h3>
-//        );
-//    }
-//});
+
 
 var About = (props) => {
     return (
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,8 +10,7 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-        <h3>Examples Component</h3>
-        <p>Welcome!</p>
+        <h3>Examples Component</h3>       
     )
 };
 
