c73dc9c378b1b0b7a64bbc2ace0b421b7f1b459f
Sarah <veleialei@gmail.com>
2017-05-15 19:10:34 -0700

    Heroku

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,22 +1,9 @@
 var React = require('react');
 
-//var WeatherMessage = React.createClass({
-//    render: function() {
-//        var{temp, location} = this.props;
-//        return (
-//            <div>
-//                <h3>{location} is {temp}</h3>     
-//            </div>
-//        );
-//    }
-//});
-
-var WeatherMessage =  ({temp, location}) => {    
-    return (
-        <div>
-            <h3>{location} is {temp}</h3>     
-        </div>
-    )
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <h3>It's it {temp} in {location}.</h3>
+  )
 };
 
 module.exports = WeatherMessage;
